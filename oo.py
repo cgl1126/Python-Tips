205c62c3153847662ff4e8b6b2cde084d6ec9fe8
caigl <caigl@vm-ubuntu.localdomain>
2019-11-21 23:00:08 +0800

    fix format

--- oo.py
+++ oo.py
@@ -13,27 +13,35 @@ def File(file_name, file_format, file_content):
         }
         return file
 
+
     def file_open(file):
         print('The {}.{} Opened.'.format(file['name'], file['format']))
+
 
     def file_read(file):
         print('Reading {}.{}...\nThe content is [{}]'.format(
             file['name'], file['format'], file['content'])
             )
 
+
     def file_close(file):
         print('The {0}.{1} Closed.'.format(file['name'], file['format']))
 
     return init(file_name, file_format, file_content)
 
-file1 = File('abc', 'txt', 'Hello world Demo')
-# print(file1)
-file1['open'](file1)
-file1['read'](file1)
-file1['close'](file1)
 
-file2 = File('music', 'mp4', 'This is song of Tay-tay.')
-# print(file2)
-file2['open'](file2)
-file2['read'](file2)
-file2['close'](file2)
+def main():
+    file1 = File('abc', 'txt', 'Hello world Demo')
+    # print(file1)
+    file1['open'](file1)
+    file1['read'](file1)
+    file1['close'](file1)
+
+    file2 = File('music', 'mp4', 'This is song of Tay-tay.')
+    # print(file2)
+    file2['open'](file2)
+    file2['read'](file2)
+    file2['close'](file2)
+
+if __name__ == '__main__':
+    main()
